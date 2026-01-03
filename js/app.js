d057cbcf0d5d75895255ece08db1008d848b7b10
Eduardo <eduardocaratori@gmail.com>
2026-01-03 14:42:02 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
+// THEME
+const themeBtn = document.getElementById("themeToggle");
+const currentTheme = localStorage.getItem("theme") || "light";
+document.documentElement.dataset.theme = currentTheme;
 
+themeBtn.onclick = () => {
+  const newTheme =
+    document.documentElement.dataset.theme === "light"
+      ? "dark"
+      : "light";
+  document.documentElement.dataset.theme = newTheme;
+  localStorage.setItem("theme", newTheme);
+};
+
+// LANGUAGE
+let lang = localStorage.getItem("lang") || "en";
+
+document.getElementById("langToggle").onclick = () => {
+  lang = lang === "en" ? "pt-BR" : "en";
+  localStorage.setItem("lang", lang);
+  loadLang();
+};
+
+async function loadLang() {
+  const res = await fetch(`i18n/${lang}.json`);
+  const data = await res.json();
+  document.getElementById("title").textContent = data.title;
+}
